2dd2a50c0d318f0871b2468aab18f00320d05a55
Capeling <113705504+Capeling@users.noreply.github.com>
2024-03-22 23:59:21 +1030

    Fix Tower BG Being Incorrectly Coloured if You Continually Scroll

--- src/hooks/HookedLevelSelectLayer.cpp
+++ src/hooks/HookedLevelSelectLayer.cpp
@@ -7,11 +7,10 @@ bool HookedLevelSelectLayer::init(int pageID) {
 ccColor3B HookedLevelSelectLayer::colorForPage(int pageID) {
     auto GM = GameManager::sharedState();
 	int colIDs[6] = { 5 ,7, 8, 9, 94, 11 };
-
-    if(pageID == 4 || pageID == 22)
-        return {37, 44, 52};
+    if(colIDs[pageID % 6] == 94)
+        return {37, 44, 52}; // The Tower
 
     return GM->colorForIdx(colIDs[pageID % 6]);
     
-    return LevelSelectLayer::colorForPage(pageID);
+    //return LevelSelectLayer::colorForPage(pageID);
 }
